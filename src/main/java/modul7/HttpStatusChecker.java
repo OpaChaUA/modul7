902248c8f33ac873976a351198b668554401cc28
OpaChaUA <opaCha.ua.94@gmail.com>
2023-05-22 21:43:23 +0300

    refactor

--- src/main/java/modul7/HttpStatusChecker.java
+++ src/main/java/modul7/HttpStatusChecker.java
@@ -2,20 +2,25 @@ package modul7;
 
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-public class HttpStatusChecker {
-    public String getStatusImage(int code) throws IOException {
-        String url = "https://http.cat/" + code + ".jpg";
-        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
-        connection.setRequestMethod("GET");
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
-        int responseCode = connection.getResponseCode();
-        if (responseCode == HttpURLConnection.HTTP_OK) {
+public class HttpStatusChecker {
+    public String getStatusImage(int code) throws IOException, InterruptedException {
+        String url = "https://http.cat/" + code + ".jpg";
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+        HttpResponse<String> respons = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        if (respons.statusCode() == 200) {
             return url;
         } else {
-            throw new IOException("Failed to retrieve image. Response code: " + responseCode);
+            throw new IOException("Failed to retrieve image. Response code:" + respons.statusCode());
         }
-
     }
+
 }
--- src/main/java/modul7/Main.java
+++ src/main/java/modul7/Main.java
@@ -2,16 +2,16 @@ package modul7;
 
 import java.io.IOException;
 
-public class Main {
-    public static void main(String[] args){
-        HttpStatusChecker checker = new HttpStatusChecker();
-        try {
-            String image200 = checker.getStatusImage(200);
-            System.out.println("200: " + image200);
 
-            String image404 = checker.getStatusImage(404);
-            System.out.println("404: " + image404);
-        } catch (IOException e) {
+public class Main {
+    public static void main(String[] args)  {
+        HttpStatusChecker httpStatusChecker = new HttpStatusChecker();
+        try {
+            System.out.println(httpStatusChecker.getStatusImage(418));
+
+        System.out.println("-------------------------------");
+        System.out.println(httpStatusChecker.getStatusImage(1000));
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
     }
